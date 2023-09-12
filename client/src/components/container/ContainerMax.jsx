a0c22c84c2b873e53bba091cbce7ac6a84369f58
abdullahbinziad <abdullahbinziad@gmail.com>
2023-09-12 22:38:41 +0600

    topbr done

---
+++ client/src/components/container/ContainerMax.jsx
@@ -0,0 +1,12 @@
+
+
+// eslint-disable-next-line react/prop-types
+const ContainerMax = ({children}) => {
+    return (
+        <div className="max-w-7xl w-5/6 mx-auto">
+            {children}
+        </div>
+    );
+};
+
+export default ContainerMax;
