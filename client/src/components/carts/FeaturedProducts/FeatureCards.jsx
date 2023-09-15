d2050a79dd61648c016a93c1fdb6d3fa43c2b580
tasniaarhamarham <tasniaarhamarham@gmail.com>
2023-09-15 16:57:09 +0600

    last update from tasnia

--- client/src/components/carts/FeaturedProducts/FeatureCards.jsx
+++ client/src/components/carts/FeaturedProducts/FeatureCards.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { CategoryTitles } from '../titles/TitlesGazi'
 import { CategoryImg } from '../avaters/Avatars'
+import { CategoryTitles } from '../../titles/Featuretitle'
 
-const FeaturedCarts = ({category}) => {
+const FeatureCards = ({category}) => {
     console.log(category)
   return (
     <div className='text-center flex flex-col justify-center items-center cursor-pointer gap-1 rounded-3xl'>
@@ -12,4 +12,4 @@ const FeaturedCarts = ({category}) => {
   )
 }
 
-export default FeaturedCarts
+export default FeatureCards
