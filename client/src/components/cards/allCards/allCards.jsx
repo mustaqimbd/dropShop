4f5cd5937570e64608fbdb0f1f54d1fe8bae35f1
abdullahbinziad <abdullahbinziad@gmail.com>
2023-09-18 18:35:16 +0600

    products load by pagination almost done

--- client/src/components/cards/allCards/allCards.jsx
+++ client/src/components/cards/allCards/allCards.jsx
@@ -1,20 +1,16 @@
 
-
-// feature product cards 
-
-import { FeaturedProductImage } from "../../avaters/Avatars"
-import { EmptyStarIcons } from "../../icons/Icons"
-
+import Rating from '@mui/material/Rating';
+import { PriceTitles } from '../../titles/FeatureTitle';
 // @ api = "./feature product.json"
 const FeatureProductCard = ({ product }) => {
     console.log(product)
-    return <div className="h-80 w-60 bg-slate-500">
-        <FeaturedProductImage src={product.images[0]?.link} title={product.product_name} ></FeaturedProductImage>
-        {
-            // <EmptyStarIcons />
-            
-
-        }
+    return <div className="  space-y-2">
+       <img className=" object-contain h-52 w-60 p-2" src={product?.images[0]?.link} alt="" />
+       <div className='flex gap-2 items-center'>
+       <Rating color='' name="read-only" value={product?.ratings} readOnly /> <span className='text-ratingCount'>(567)</span>
+       </div>
+       <h1 className=' font-semibold text-heading '>{product?.product_name.slice(0,40)} { product?.product_name.length>40 ? '...': '' } </h1>
+       <p className='font-semibold TEXT-NORMAL text-priceText'> ৳ {product?.reseller_price}</p>
     </div>
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css'
+import "./index.css";
 
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/Routes.jsx';
-import { ThemeProvider } from '@mui/material';
-import theme from './Theme/Theme.jsx';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Routes.jsx";
+import { ThemeProvider } from "@mui/material";
+import theme from "./Theme/Theme.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <ThemeProvider theme={theme}>
-  <RouterProvider router={router}>
-
-  </RouterProvider>
-  </ThemeProvider>
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
