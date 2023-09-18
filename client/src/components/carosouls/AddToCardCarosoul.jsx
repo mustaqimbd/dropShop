db721753e4080afeb6c7c784ea081a0e5d84be41
Gazi Ehsanul Haque <antik.edu@gmail.com>
2023-09-18 14:27:58 +0600

    main files

--- client/src/components/carosouls/AddToCardCarosoul.jsx
+++ client/src/components/carosouls/AddToCardCarosoul.jsx
@@ -1,37 +1,20 @@
 import React from 'react'
 import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const AddToCardCarosoul = () => {
     const onChange = () => { }
     const onClickItem = () => { }
     const onClickThumb = () => { }
-    
+
     return (
+        
         <Carousel showArrows={true} onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb}>
-            <div>
-                <img src="assets/1.jpeg" />
-                <p className="legend">Legend 1</p>
-            </div>
-            <div>
-                <img src="assets/2.jpeg" />
-                <p className="legend">Legend 2</p>
-            </div>
-            <div>
-                <img src="assets/3.jpeg" />
-                <p className="legend">Legend 3</p>
-            </div>
-            <div>
-                <img src="assets/4.jpeg" />
-                <p className="legend">Legend 4</p>
-            </div>
-            <div>
-                <img src="assets/5.jpeg" />
-                <p className="legend">Legend 5</p>
-            </div>
-            <div>
-                <img src="assets/6.jpeg" />
-                <p className="legend">Legend 6</p>
-            </div>
+            
+            <div><img src="files/tv-1.jpg" /></div>
+            <div><img src="files/tv-2.jpg" /></div>
+            <div><img src="files/head-1.jfif" /></div>
+            <div><img src="files/head-2.webp" /></div>
         </Carousel>
     )
 }
