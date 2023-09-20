f5fb17bc6da03ac38d170878494a0006895e4d0f
abdullahbinziad <abdullahbinziad@gmail.com>
2023-09-21 00:30:07 +0600

    working on categorised products

--- client/src/pages/home/FeaturedProducts/FeatureProducts.jsx
+++ client/src/pages/home/FeaturedProducts/FeatureProducts.jsx
@@ -3,19 +3,16 @@ import ContainerMax from "../../../components/container/ContainerMax";
 import { FeatureProduct } from "../../../components/titles/FeatureTitle";
 import { FeatureProductCard } from "../../../components/cards/allCards/allCards";
 import useAllProducts from "../../../hooks/useAllProducts";
+import Pagination from "../../../components/pagination/Pagination";
 
 const FeatureProducts = () => {
   const itemsPerPage = 24; // Number of items per page
+  const allProductsLength = 95;
   const [currentPage, setCurrentPage] = useState(0);
-  // Default state is an empty array
   const { allProducts, isLoading } = useAllProducts(currentPage, itemsPerPage);
   const allProductsData = allProducts.products;
-  const allProductsLength = 95;
-  // Calculate total pages and indices for pagination
-  const totalPages = Math.ceil(allProductsLength / itemsPerPage);
 
-  console.log("total page:", totalPages);
-  console.log("current page page:", currentPage);
+  //
 
   return (
     <ContainerMax>
@@ -33,48 +30,12 @@ const FeatureProducts = () => {
         )}
 
         {allProductsData?.length ? (
-          <div className="mt-8 flex justify-center items-center space-x-4">
-            <button
-              onClick={() => setCurrentPage(currentPage - 1)}
-              disabled={currentPage === 0}
-              className={`px-4 py-2 rounded-md ${
-                currentPage === 0
-                  ? "bg-gray-300 text-gray-600 cursor-not-allowed"
-                  : "bg-primary text-white hover:bg-blue-600"
-              }`}
-            >
-              Previous
-            </button>
-
-            {/* Page numbers */}
-            <div className="flex space-x-2">
-              {[...Array(totalPages).keys()].map((page) => (
-                <button
-                  key={page}
-                  onClick={() => setCurrentPage(page)}
-                  className={`px-3 py-2 rounded-full ${
-                    currentPage === page
-                      ? "bg-primary text-white"
-                      : "bg-gray-200 text-gray-800 hover:bg-blue-200"
-                  }`}
-                >
-                  {page + 1}
-                </button>
-              ))}
-            </div>
-
-            <button
-              onClick={() => setCurrentPage(currentPage + 1)}
-              disabled={currentPage + 1 === totalPages}
-              className={`px-4 py-2 rounded-md ${
-                currentPage + 1 === totalPages
-                  ? "bg-gray-300 text-gray-600 cursor-not-allowed"
-                  : "bg-primary text-white hover:bg-blue-600"
-              }`}
-            >
-              Next
-            </button>
-          </div>
+          <Pagination
+            allProductsLength={allProductsLength}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            itemsPerPage={itemsPerPage}
+          />
         ) : (
           ""
         )}
---
+++ client/src/pages/products/ArchiveProducts/FilteredProducts.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { CategoriesProductCard } from "../../../components/cards/allCards/allCards";
+import ContainerFull from "../../../components/container/ContainerFull";
+
+import Pagination from "../../../components/pagination/Pagination";
+
+import useAllProducts from "../../../hooks/useAllProducts";
+
+const FilteredProducts = () => {
+  const itemsPerPage = 24; // Number of items per page
+  const allProductsLength = 95;
+  const [currentPage, setCurrentPage] = useState(0);
+  const { allProducts, isLoading } = useAllProducts(currentPage, itemsPerPage);
+  const allProductsData = allProducts.products;
+
+  //
+
+  return (
+    <ContainerFull>
+      <div className="space-y-5">
+        {isLoading ? (
+          <p>Loading....</p>
+        ) : (
+          <div className="grid   grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-4    gap-8">
+            {allProductsData?.map((product) => (
+              <CategoriesProductCard key={product._id} product={product} />
+            ))}
+          </div>
+        )}
+
+        <div className="flex justify-end">
+          {allProductsData?.length ? (
+            <Pagination
+              allProductsLength={allProductsLength}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+              itemsPerPage={itemsPerPage}
+            />
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </ContainerFull>
+  );
+};
+
+export default FilteredProducts;
