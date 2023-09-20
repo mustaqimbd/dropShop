c3da43b80fe0586f104950e91efca9cf50ebab16
tasniaarhamarham <tasniaarhamarham@gmail.com>
2023-09-21 04:13:48 +0600

    order_id send

--- client/src/pages/TrackOrder/TrackOrder.jsx
+++ client/src/pages/TrackOrder/TrackOrder.jsx
@@ -5,10 +5,12 @@ import ContainerFull from "../../components/container/ContainerFull";
 import ContainerMax from "../../components/container/ContainerMax";
 import { TracOrdertitle } from "../../components/titles/FeatureTitle";
 import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled';
-import TrackCard from "../../components/cards/TrackCard/TrackCard";
 import { BackToOrderBtn } from "../../components/buttons/Buttons";
 import { Link } from "react-router-dom";
+import HighLightedProductsCard from "../../components/cards/highLightSectionCard/HighLightedProductsCard";
+import {  useForm } from "react-hook-form";
 const TrackOrder = () => {
+  const[orderId,setOrderId]=useState(null)
     const[Track,setTrack]=useState([])
            useEffect(() => {
            fetch("http://localhost:5000/api/products/highlight-products")
@@ -17,13 +19,28 @@ const TrackOrder = () => {
            setTrack(data.payload.topSellingProducts);
           });
       }, []);
-    let id ="OD23244545425"
+
+    const { register, handleSubmit,  reset } = useForm();
+      const handleSearch = (data) => {
+      console.log( data.title);
+      setOrderId(data.title);
+      reset();
+    };
     return (
 <ContainerFull>
 <div className="bg-gray py-5 border-b-2 border-borderColor px-14">
 <ContainerMax>
+
 <TracOrdertitle title={"My Orders/Tracking"}/>
-<div className="text-base font-sans text-heading  py-5 ">Order ID : {id}</div>
+{/* <div className="text-base font-sans text-heading  py-5 ">Order ID : {orderId}</div> */}
+{/* submit order id */}
+<form onSubmit={handleSubmit(handleSearch)} className="py-5">
+      
+      
+      <input type="text"className="border border-1 py-2 px-4" id="title"placeholder="Enter Order Id" {...register("title")} />
+
+        <button type="submit"className="btn bg-priceText text-white py-2 px-4 ">Search</button>
+      </form>
    <div className="flex justify-between gap-4 font-sans border border-borderColor p-4 rounded-lg">
 <TrackItem information_name={"Estimated Delivery time:"} information={"24 Nov 2022"}/>
 <TrackItem information_name={"Shipping By:"} information={ 
@@ -35,13 +52,13 @@ const TrackOrder = () => {
 <TrackItem information_name={"Tracking#:"} information={"BD04535352345"}/>
 </div>
 <div className="w-full py-12">
-<CustomizedSteppers orderid={id}/>
+<CustomizedSteppers orderid={orderId}/>
 </div>
 
 <div className="flex justify-between gap-4 font-sans border border-borderColor p-4">
   {
     Track.map(item => (
-      <TrackCard key={item._id} content={item} />
+      <HighLightedProductsCard key={item._id} content={item} />
     ))
   }
 </div>
