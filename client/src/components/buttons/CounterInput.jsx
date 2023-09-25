d1cbe05908bdc44ca7867c9bcdf0b312e86c6bbe
Gazi Ehsanul Haque <antik.edu@gmail.com>
2023-09-25 16:13:53 +0600

    products table working

--- client/src/components/buttons/CounterInput.jsx
+++ client/src/components/buttons/CounterInput.jsx
@@ -1,14 +1,17 @@
+import { Button } from '@mui/material'
 import React, { useEffect, useState } from 'react'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Hidden from '@mui/material/Hidden';
 
-const CounterInput = ({handleIncrement, handleDecrement, quantity, productAvailable}) => {
+const CounterInput = ({ handleIncrement, handleDecrement, quantity, productAvailable }) => {
     const [maxLimit, setMaxLimit] = useState(false)
-    
+
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 items-center'>
             <div className='outline-2 outline-slate-500'>
                 <div className="custom-number-input h-10 w-32">
                     <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-                        <button data-action="decrement" className="  text-gray-600   h-full w-20 rounded-l cursor-pointer outline-none" onClick={handleDecrement} disabled={quantity==0}>
+                        <button data-action="decrement" className="  text-gray-600   h-full w-20 rounded-l cursor-pointer outline-none" onClick={handleDecrement} disabled={quantity == 0}>
                             <span className="m-auto text-2xl font-thin">−</span>
                         </button>
                         <input type="number" className=" focus:outline-none text-center w-full  font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none" name="custom-input-number" value={quantity}></input>
@@ -18,11 +21,29 @@ const CounterInput = ({handleIncrement, handleDecrement, quantity, productAvaila
                     </div>
                 </div>
             </div>
-            <div>
-                Add to card section
+            <div >
+            <Button
+      variant="contained"
+      color="warning"
+      style={{
+        color: 'white',
+        width:'100%' // Vertically center the content
+      }}
+    >
+      Add to Cart
+      <ShoppingCartIcon style={{ marginLeft: '8px' }} />
+    </Button>
+
             </div>
             <div>
-
+                <Button
+                    variant="outlined"
+                    color="warning" // Customize color as "orange" (Material-UI warning color)
+                    fullWidth // Make the button span the full width
+                    style={{ color: 'orange', borderColor: 'orange' }} // Customize text and border color
+                >
+                    Buy Now
+                </Button>
             </div>
         </div>
     )
--- client/src/pages/Dashboard/Admin/Products/AdminProductPageBody.jsx
+++ client/src/pages/Dashboard/Admin/Products/AdminProductPageBody.jsx
@@ -1,51 +1,63 @@
 import React from 'react'
+// import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(name, calories, fat, carbs, protein, cobi) {
+  return { name, calories, fat, carbs, protein, cobi };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 70),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 40),
+  createData('Eclair', 262, 16.0, 24, 6.0, 30),
+  createData('Cupcake', 305, 3.7, 67, 4.3, 22),
+  createData('Gingerbread', 356, 16.0, 49, 3.9, 12),
+  createData('cobi', 356, 16.0, 49, 3.9, 25),
+];
+
 
 const AdminProductPageBody = () => {
   return (
     <div>
-      <div className="overflow-x-auto">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th>Offer</th>
-              <th>Purchased</th>
-              <th>Stocked</th>
-              <th>Status</th>
-              <th>Date</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <td>
-                <div className="flex items-center ">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td> Zemlak, Daniel and Leannon</td>
-              <td>Price</td>
-              <td>Offer</td>
-              <td>Purchased</td>
-              <td>Stocked</td>
-              <td>Status</td>
-              <td>Date</td>
-              <th>
-                <button className="btn btn-info btn-xs">Edit</button>
-                <button className='btn btn-warning btn-xs'>Delete</button>
-              </th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+        <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Products</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Offer</TableCell>
+            <TableCell align="right">Purchased</TableCell>
+            <TableCell align="right">Stock</TableCell>
+
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.cobi}</TableCell>
+
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   )
 }
